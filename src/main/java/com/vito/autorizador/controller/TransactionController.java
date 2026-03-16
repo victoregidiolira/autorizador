b0042fe1d523153861b3cb12f9a7d96062d3eade
victoregidiolira <viegidulira@gmail.com>
2026-03-16 10:55:38 -0300

    feat: script added and working.

--- src/main/java/com/vito/autorizador/controller/TransactionController.java
+++ src/main/java/com/vito/autorizador/controller/TransactionController.java
@@ -3,11 +3,13 @@ package com.vito.autorizador.controller;
 import com.vito.autorizador.dto.TransactionRequest;
 import com.vito.autorizador.service.TransactionService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/transactions")
 public class TransactionController {
